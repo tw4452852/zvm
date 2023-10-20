b1e065daf73816cc904f254d5c95d948427e9e13
Tw <tw19881113@gmail.com>
2023-10-20 16:31:14 +0800

    arm/pci: remove ugly hack due to packed structure
    Signed-off-by: Tw <tw19881113@gmail.com>

--- src/arch/arm/pci.zig
+++ src/arch/arm/pci.zig
@@ -8,18 +8,19 @@ const libfdt = @cImport({
 });
 const check = @import("../../helpers.zig").check_non_zero;
 
-const UnitAddr = packed struct {
+// we use extern here to preserve fields order
+const UnitAddr = extern struct {
     hi: u32,
     mid: u32,
     lo: u32,
 };
 
-const Irq = packed struct {
+const Irq = extern struct {
     pci_addr: UnitAddr,
     pci_pin: u32,
 };
 
-const GicIrq = packed struct {
+const GicIrq = extern struct {
     phandle: u32,
     addr_hi: u32,
     addr_lo: u32,
@@ -28,15 +29,18 @@ const GicIrq = packed struct {
     flags: u32,
 };
 
-const IrqMap = packed struct {
+const IrqMap = extern struct {
     pci_irq: Irq,
     gic_irq: GicIrq,
 };
 
-const Range = packed struct {
+const Range = extern struct {
     pci_addr: UnitAddr,
-    cpu_addr: u64,
-    length: u64,
+    // we have to split u64 to avoid padding here because UnitAddr is 32bit aligned
+    cpu_addr_hi: u32,
+    cpu_addr_lo: u32,
+    length_hi: u32,
+    length_lo: u32,
 };
 
 const AddrType = enum(u32) {
@@ -56,16 +60,17 @@ pub fn generate_fdt_node(dts: ?*anyopaque) !void {
                 .mid = libfdt.cpu_to_fdt32(pci.addrspace_start >> 32),
                 .lo = libfdt.cpu_to_fdt32(pci.addrspace_start & 0xffffffff),
             },
-            .cpu_addr = libfdt.cpu_to_fdt64(mmio.GAP_START + mmio.GAP_SIZE),
-            .length = libfdt.cpu_to_fdt64(pci.addrspace_size),
+            .cpu_addr_lo = libfdt.cpu_to_fdt32(@truncate(mmio.GAP_START + mmio.GAP_SIZE)),
+            .cpu_addr_hi = libfdt.cpu_to_fdt32((mmio.GAP_START + mmio.GAP_SIZE) >> 32),
+            .length_lo = libfdt.cpu_to_fdt32(@truncate(pci.addrspace_size)),
+            .length_hi = libfdt.cpu_to_fdt32(pci.addrspace_size >> 32),
         },
     };
-    var buf: [4096]u8 = undefined;
-    var fbs = std.io.fixedBufferStream(&buf);
-    const wr = fbs.writer();
+
+    var irq_maps: [64]IrqMap = undefined;
     const pci_devs = pci.registered_devs();
     for (pci_devs, 0..) |d, i| {
-        try wr.writeAll(std.mem.asBytes(&IrqMap{
+        irq_maps[i] = .{
             .pci_irq = .{
                 .pci_addr = .{
                     .hi = libfdt.cpu_to_fdt32(@as(u32, @intCast(i << 11))), // device number
@@ -82,7 +87,7 @@ pub fn generate_fdt_node(dts: ?*anyopaque) !void {
                 .num = libfdt.cpu_to_fdt32(d.cfg.comm.irq_line - gic.irq_spi_base),
                 .flags = libfdt.cpu_to_fdt32(gic.irq_level_high),
             },
-        })[0 .. @bitSizeOf(IrqMap) / 8]);
+        };
     }
     const irq_mask: Irq = .{
         .pci_addr = .{
@@ -102,12 +107,11 @@ pub fn generate_fdt_node(dts: ?*anyopaque) !void {
     try check(libfdt.fdt_property_cell(dts, "#interrupt-cells", 1));
     try check(libfdt.fdt_property(dts, "reg", &cfg_reg, @sizeOf(@TypeOf(cfg_reg))));
     try check(libfdt.fdt_property(dts, "bus-range", &bug_range, @sizeOf(@TypeOf(bug_range))));
-    try check(libfdt.fdt_property(dts, "ranges", &ranges, @bitSizeOf(@TypeOf(ranges)) / 8));
+    try check(libfdt.fdt_property(dts, "ranges", &ranges, @sizeOf(@TypeOf(ranges))));
     try check(libfdt.fdt_property_cell(dts, "msi-parent", gic.msi_phandle));
     if (pci_devs.len > 0) {
-        const written = fbs.getWritten();
-        try check(libfdt.fdt_property(dts, "interrupt-map", written.ptr, @intCast(written.len)));
-        try check(libfdt.fdt_property(dts, "interrupt-map-mask", &irq_mask, @bitSizeOf(@TypeOf(irq_mask)) / 8));
+        try check(libfdt.fdt_property(dts, "interrupt-map", &irq_maps, @intCast(@sizeOf(@TypeOf(irq_maps[0])) * pci_devs.len)));
+        try check(libfdt.fdt_property(dts, "interrupt-map-mask", &irq_mask, @sizeOf(@TypeOf(irq_mask))));
     }
 
     try check(libfdt.fdt_end_node(dts));
